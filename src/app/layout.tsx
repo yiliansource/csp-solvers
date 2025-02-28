92f8e3641956e3f3f5279b6096e3b900f72d6a83
Ian Hornik <yiliansource@gmail.com>
2025-03-01 00:04:32 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <meta property="og:image" content="/apple-touch-icon.png" />
                 <meta name="theme-color" content="#ee5050" />
             </head>
             <body
