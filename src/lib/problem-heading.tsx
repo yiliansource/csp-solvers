f06ecc512cbd48410c46dab50fdbf686d6f8c330
Ian Hornik <yiliansource@gmail.com>
2025-03-01 12:29:32 +0100

    animate problem header

--- src/lib/problem-heading.tsx
+++ src/lib/problem-heading.tsx
@@ -1,12 +1,24 @@
+import * as motion from "motion/react-client";
+
 export function ProblemHeading({ title }: { title: string }) {
     return (
-        <div className="mb-6 lg:mb-0 select-none">
-            <div className="hidden lg:block absolute right-0 -top-26">
+        <motion.div className="mb-6 lg:mb-0 select-none">
+            <motion.div
+                className="hidden lg:block absolute right-0 -top-26"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+            >
                 <h1 className="text-3xl font-bold">{title}</h1>
-            </div>
-            <div className="block lg:hidden absolute -top-12">
+            </motion.div>
+            <motion.div
+                className="block lg:hidden absolute -top-12"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+            >
                 <h1 className="text-2xl font-bold">{title}</h1>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     );
 }
